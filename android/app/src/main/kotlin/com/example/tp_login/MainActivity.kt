437ebf179226a9b9ad1823a9ab0a36a86c21494f
Badr Moutaouakil <b.moutawakil.at@gmail.com>
2025-01-10 20:22:26 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/tp_login/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tp_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:  const Text('Flutter TP Login',
+              style: TextStyle(color: Color.fromARGB(255, 3, 17, 27))),
+              backgroundColor: const Color.fromARGB(255, 45, 213, 36),
+              centerTitle: true,
+
+        ),
+        body: Container( child: Column(children: [
+
+            Expanded(
+              flex: 1,
+              child: Container(),),
+
+            Expanded(
+              flex: 5,
+              child:Row(children: [Expanded(flex: 1, child: Container(),),
+              Expanded(flex: 6, child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Colors.red,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 5.0,
+                  )
+                  
+                ),
+                child: Column(children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    child: Image.asset("images/2142f4c64fbeace90bb0ce1845df603b.png",
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                    
+                 
+                    ),
+
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(15),
+                     child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Username',
+                      suffixIcon : Icon(Icons.email),
+ 
+                  ))
+                    ),
+                 Container(
+                    padding: EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 15),
+                     child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Password',
+                      suffixIcon : Icon(Icons.lock),
+ 
+                  ))
+                    )
+                 
+
+                ]),
+              
+                
+                ),),
+                Expanded(flex: 1, child: Container(),),]),
+              ),
+
+              
+            Expanded(
+              flex: 1,
+              child: Container(),)
+            
+          ],
+        )
+
+          
+
+        )
+      ),
+    );
+  }
+}
